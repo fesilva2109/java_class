e467cedb13e8b1913040278f6f151a9bb099a98f
fesilva2109 <felipesilvaa.2109@gmail.com>
2025-03-11 21:52:09 -0300

    Adicionando Title, Description e Version

--- src/main/java/br/com/fiap/api_rest/ApiRestApplication.java
+++ src/main/java/br/com/fiap/api_rest/ApiRestApplication.java
@@ -1,9 +1,12 @@
 package br.com.fiap.api_rest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API de Livros - Made by Felipe Maciel", description = "Exemplo de API RESTful com Swagger da 2TDSPW", version = "v1"))
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
